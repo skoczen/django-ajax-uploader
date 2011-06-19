da7611e6cff524075c4d0521bf854062b3501315
Henrique Romano <chromano@gmail.com>
2011-06-19 15:25:20 -0300

    Adds a 'path' variable pointing to the place in the server where file was stored

--- ajaxuploader/backends/local.py
+++ ajaxuploader/backends/local.py
@@ -10,15 +10,17 @@ class LocalUploadBackend(AbstractUploadBackend):
     UPLOAD_DIR = "uploads"
 
     def setup(self, filename):
-        self._filename = os.path.join(
-            os.path.join(settings.MEDIA_ROOT, self.UPLOAD_DIR), filename)
-
+        self._path = os.path.join(
+            settings.MEDIA_ROOT, self.UPLOAD_DIR, filename)
         try:
-            os.makedirs(os.path.realpath(os.path.dirname(self._filename)))
+            os.makedirs(os.path.realpath(os.path.dirname(self._path)))
         except:
             pass
-
-        self._dest = BufferedWriter(FileIO(self._filename, "wb"))
+        self._dest = BufferedWriter(FileIO(self._path, "wb"))
 
     def upload_chunk(self, chunk):
         self._dest.write(chunk)
+
+    def upload_complete(self, request, filename):
+        path = settings.MEDIA_URL + self.UPLOAD_DIR + "/" + filename
+        return {"path": path}
