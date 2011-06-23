0ab38abf170f4331c64e367550249390e9d6a164
Chris Jones <chris@brack3t.com>
2011-06-23 15:51:08 -0700

    Added some docstrings. Using mimetypes to guess mimetype of file being uploaded. Collection is now optional and will default to fs if not specified. Moved connection setup into the setup method instead of init.

--- ajaxuploader/backends/mongodb.py
+++ ajaxuploader/backends/mongodb.py
@@ -1,3 +1,5 @@
+import mimetypes
+
 from django.conf import settings
 
 from pymongo import Connection
@@ -9,17 +11,37 @@ class MongoDBUploadBackend(AbstractUploadBackend):
 
     def __init__(self, *args, **kwargs):
         self.db = kwargs.pop('db')
-        self.collection = kwargs.pop('collection')
+
+        try:
+            self.collection = kwargs.pop('collection')
+        except KeyError:
+            self.collection = None
+
+        super(MongoDBUploadBackend, self).__init__(*args, **kwargs)
+
+    def setup(self, filename, encoding='UTF-8'):
+        """
+        Setup MongoDB connection to specified db. Collection is optional
+        and will default to fs if not specified.
+
+        Create new gridFS file which returns a GridIn instance to write
+        data to.
+        """
         self.connection = Connection(
             host=getattr(settings, 'AJAXUPLOAD_MONGODB_HOST', 'localhost'),
             port=getattr(settings, 'AJAXUPLOAD_MONGODB_PORT', 27017)
         )[self.db]
-        self.grid = gridfs.GridFS(self.connection, collection=self.collection)
-        super(MongoDBUploadBackend, self).__init__(*args, **kwargs)
 
-    def setup(self, filename):
+        if self.collection:
+            self.grid = gridfs.GridFS(self.connection,
+                collection=self.collection)
+        else:
+            self.grid = gridfs.GridFS(self.connection)
+
         self._path = filename
-        self.f = self.grid.new_file(**{'filename': self._path})
+        content_type, encoding = mimetypes.guess_type(filename)
+        self.f = self.grid.new_file(**{'filename': self._path,
+            'encoding': 'UTF-8', 'content_type': content_type})
 
     def upload_chunk(self, chunk):
         self.f.write(chunk)
