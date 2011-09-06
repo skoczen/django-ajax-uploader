5dd9ecb736697b5b0af511272247bc1029ed6bd6
Istergul <istergul@gmail.com>
2011-09-06 23:03:04 +0400

    Added use tempfile in local backend

--- ajaxuploader/backends/local.py
+++ ajaxuploader/backends/local.py
@@ -2,6 +2,8 @@ from io import FileIO, BufferedWriter
 import os
 
 from django.conf import settings
+from django.core.files.temp import NamedTemporaryFile
+from django.core.files.move import file_move_safe
 
 from ajaxuploader.backends.base import AbstractUploadBackend
 
@@ -15,12 +17,11 @@ class LocalUploadBackend(AbstractUploadBackend):
             os.makedirs(os.path.realpath(os.path.dirname(self._path)))
         except:
             pass
-        self._dest = BufferedWriter(FileIO(self._path, "w"))
+        self._dest = NamedTemporaryFile(suffix='.upload', dir=settings.FILE_UPLOAD_TEMP_DIR)
 
     def upload_chunk(self, chunk):
         self._dest.write(chunk)
 
     def upload_complete(self, request, filename):
-        path = settings.MEDIA_URL + self.UPLOAD_DIR + "/" + filename
-        self._dest.close()
-        return {"path": path}
+        file_move_safe(self._dest.name, self._path, allow_overwrite=True)
+        return {"path": self._path}
