fee84db6ac8ce7ece464312cfc65e8fe55f4dfb7
Diego Andres Sanabria Martin <diegueus9@gmail.com>
2011-10-12 11:56:50 -0500

    Initial version of model for record uploaded files

---
+++ ajaxuploader/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes import generic
+
+class UploadedFile(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    path = models.CharField(max_length=400)
