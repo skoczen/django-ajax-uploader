06acb0635110fc49a009319ec1a2df7985e9f0e9
Diego Andres Sanabria Martin <diegueus9@gmail.com>
2011-10-12 12:04:15 -0500

    Allowed null content_object

--- ajaxuploader/models.py
+++ ajaxuploader/models.py
@@ -3,7 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
 class UploadedFile(models.Model):
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
     content_object = generic.GenericForeignKey('content_type', 'object_id')
     path = models.CharField(max_length=400)
