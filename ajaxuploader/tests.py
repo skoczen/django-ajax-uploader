d25f853ae639cc2e1f16ac35f281fe9312c23284
Florian Hahn <flo@fhahn.com>
2011-11-11 22:05:41 +0100

    added test for uploading a file to LocalUploadBackend

--- ajaxuploader/tests.py
+++ ajaxuploader/tests.py
@@ -1,13 +1,11 @@
 import os
+import hashlib
 from shutil import rmtree
 
-from django.core.files.storage import default_storage
-from django.contrib.auth.models import User
 from django.conf import settings
 from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
-
 
 
 class AjaxUploadTest(TestCase):
@@ -15,31 +13,49 @@ class AjaxUploadTest(TestCase):
 
     def setUp(self):
         super(AjaxUploadTest, self).setUp()
-        if hasattr(settings, 'USER_UPLOADS'):
-            self.old_upload_dir = settings.USER_UPLOADS
-        else:
-            self.old_upload_dir = None
-
-        settings.USER_UPLOADS = 'tests/uploads/'
         
         self.test_dir = os.path.dirname(__file__)
-        test_file = open(os.path.join(self.test_dir, 'fixtures/pony.png'), 'rb')
-        self.test_file_1 = test_file
-        User.objects.create_user(username='user1',
-                                          password='!', email='foo@bar.com')
+        test_file = open(os.path.join(self.test_dir, 'fixtures/pony.png'),
+                         'rb')
 
+        self.test_file_1 = test_file
+
+        # generate sha1 hash of the file
+        self.test_file_1_hash = hashlib.sha1(self.test_file_1.read())\
+                                       .hexdigest()
+
+        # reset position to beginning of the file
+        self.test_file_1.seek(0)
+        
         
     def tearDown(self):
-        #rmtree(os.path.join(settings.MEDIA_ROOT, settings.USER_UPLOADS))
-        settings.USER_UPLOADS = self.old_upload_dir
+        # remove created uploads/tests directory
+         rmtree(os.path.join(settings.MEDIA_ROOT, 'uploads/tests'))
 
-    def test_upload_view(self):
+    def test_upload_raw_post_local_backend(self):
         """
-        test the ajax upload view
+        tests uploading a file to LocalUploadBackend
         """
-        # there is a bug in Django 1.3 with raw_post_data in the django test client
-        # https://code.djangoproject.com/ticket/15679
-        # fixed in 1.3.1 
 
-        response = self.client.post(reverse('ajax-upload')+'?qqfile=foobar.png', self.test_file_1.read(), content_type='application/octet-stream', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        # there is a bug in Django 1.3.1 with raw_post_data in the django test client
+        # it's fixed in trunk
+        # https://code.djangoproject.com/changeset/16479
 
+        uploaded_file_name = 'tests/foo.png'
+
+        # post raw self.test_file_1 data to AjaxFileUploader as Ajax request
+        response = self.client.post(reverse('ajax-upload')+'?qqfile=%s' % \
+                                    uploaded_file_name, self.test_file_1.read(),
+                                    content_type='application/octet-stream',
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        uploaded_file_path = os.path.join(settings.MEDIA_ROOT, 'uploads',
+                                          uploaded_file_name)
+        uploaded_file = open(uploaded_file_path, 'rb')
+
+        # uploaded file must exist in MEDIA_DIR
+        self.assertTrue(os.path.exists(uploaded_file_path))
+
+        # sha1 hash of original file and uploaded file must match
+        self.assertEquals(hashlib.sha1(uploaded_file.read()).hexdigest(),
+                          self.test_file_1_hash)
