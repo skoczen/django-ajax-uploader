3a4a829b924ac73eef586ab3eb60b904212d1a26
Florian Hahn <flo@fhahn.com>
2011-11-12 22:19:05 +0100

    use default_storage to save local files

--- ajaxuploader/backends/local.py
+++ ajaxuploader/backends/local.py
@@ -1,26 +1,33 @@
-from io import FileIO, BufferedWriter
 import os
+from io import FileIO, BufferedWriter
 
-from django.conf import settings
+
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.template.loader import render_to_string
 
 from ajaxuploader.backends.base import AbstractUploadBackend
 
+
 class LocalUploadBackend(AbstractUploadBackend):
-    UPLOAD_DIR = "uploads"
+    UPLOAD_DIR = 'uploads'
 
     def setup(self, filename):
-        self._path = os.path.join(
-            settings.MEDIA_ROOT, self.UPLOAD_DIR, filename)
-        try:
-            os.makedirs(os.path.realpath(os.path.dirname(self._path)))
-        except:
-            pass
-        self._dest = BufferedWriter(FileIO(self._path, "w"))
+        # join UPLOAD_DIR with filename 
+        new_path = os.path.join(self.UPLOAD_DIR, filename)
+
+        # save empty file in default storage with path = new_path
+        self._path = default_storage.save(new_path, ContentFile(''))
+
+        # get absolute path to new file
+        self._abs_path = default_storage.path(self._path)
+
+        # create BufferedWriter for new file
+        self._dest = BufferedWriter(FileIO(self._abs_path, "w"))
 
     def upload_chunk(self, chunk):
         self._dest.write(chunk)
 
     def upload_complete(self, request, filename):
-        path = settings.MEDIA_URL + self.UPLOAD_DIR + "/" + filename
         self._dest.close()
-        return {"path": path}
+        return {"path": self._path}
