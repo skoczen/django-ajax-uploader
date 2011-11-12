a022352c26f64983f3d1fdb0e91a2f7259e86d93
Florian Hahn <flo@fhahn.com>
2011-11-12 22:37:01 +0100

    removed BufferedWriter, use django.core.files.File.write()

--- ajaxuploader/backends/local.py
+++ ajaxuploader/backends/local.py
@@ -1,6 +1,4 @@
 import os
-from io import FileIO, BufferedWriter
-
 
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
@@ -17,17 +15,14 @@ class LocalUploadBackend(AbstractUploadBackend):
         new_path = os.path.join(self.UPLOAD_DIR, filename)
 
         # save empty file in default storage with path = new_path
-        self._path = default_storage.save(new_path, ContentFile(''))
-
-        # get absolute path to new file
-        self._abs_path = default_storage.path(self._path)
+        self.path = default_storage.save(new_path, ContentFile(''))
 
         # create BufferedWriter for new file
-        self._dest = BufferedWriter(FileIO(self._abs_path, "w"))
+        self._dest = default_storage.open(self.path, mode='wb')
 
     def upload_chunk(self, chunk):
         self._dest.write(chunk)
 
     def upload_complete(self, request, filename):
         self._dest.close()
-        return {"path": self._path}
+        return {"path": self.path}
