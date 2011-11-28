4c14c4babf26e912633ef9335295ac5f50d07ef7
Florian Hahn <flo@fhahn.com>
2011-11-28 23:09:40 +0100

    moved urls.py to tests/urls.py

--- ajaxuploader/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-from ajaxuploader.views import AjaxFileUploader
-
-
-urlpatterns = patterns('',
-    url(r'^upload$', AjaxFileUploader(), name="ajax-upload"),                
-)
